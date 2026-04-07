6333f3c4d2b09e8e09f12503ca81197bb35cd58e
EvertBunschoten <evertbun@xs4all.nl>
2026-04-07 21:54:25 +0200

    Update docker entrypoint file and scalers for MLPCpp compatibility

--- docker/build/runTests.sh
+++ docker/build/runTests.sh
@@ -66,7 +66,7 @@ else
   cd src/SU2_DataMiner
   export SU2DATAMINER_HOME=$PWD
 fi
-
+export TF_ENABLE_ONEDNN_OPTS=0
 
 # Activate python virtual environment and install required modules
 echo "Installing python modules..."
